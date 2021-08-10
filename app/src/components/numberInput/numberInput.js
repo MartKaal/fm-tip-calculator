22e7711057fa8774e25a36e776aaab69d5c2ae18
Murrt <48913272+MartKaal@users.noreply.github.com>
2021-08-10 11:06:49 +0200

    Calc
    Add the functional parts of the project

--- app/src/components/numberInput/numberInput.js
+++ app/src/components/numberInput/numberInput.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import './numberInput.scss';
 
 export default class NumberInput extends Component {
 	static propTypes = {
 		id: PropTypes.string.isRequired,
 		locked: PropTypes.bool,
 		focussed: PropTypes.bool,
-		value: PropTypes.number,
+		value: PropTypes.string,
 		error: PropTypes.string,
 		label: PropTypes.string,
 		placeholder: PropTypes.string,
@@ -29,6 +28,7 @@ export default class NumberInput extends Component {
 		this.state = {
 		  focussed: false,
 		  error: false,
+		  errorMessage: '',
 		};
 	}
 	
@@ -36,22 +36,30 @@ export default class NumberInput extends Component {
 
 		// Check if people has zero
 		if(event.target.id === 'people' && event.target.value === '0') {
-			this.setState({error: true})
+			this.setState({error: true, errorMessage: `Can't be zero`})
 		} else if(event.target.id === 'people' && event.target.value !== '0') {
-			this.setState({error: false})
+			this.setState({error: false, errorMessage: ''})
 		}
 		
 	}
+
+	preventMinus = (e) => {
+		if (e.code === 'Minus') {
+			e.preventDefault();
+		}
+	};
 	
 	render() {
-		const { focussed, error } = this.state;
+		const { focussed, error, errorMessage } = this.state;
 		const { id, value, label, placeholder} = this.props;
 
-		const fieldClassName = `field ${focussed ? 'focussed' : ''}`;
+		const fieldClassName = `field ${focussed ? 'focussed' : ''} ${error ? 'error' : ''} ${id}`;
 
 		return(
-			<>	
-				{ label.length > 0 && <label htmlFor={id}>{label}</label> }
+			<>	<div className="input__labels">
+					{ label.length > 0 && <label htmlFor={id}>{label}</label> }
+					{ error === true && <span>{errorMessage}</span> }
+				</div>
 				<div className={fieldClassName}>
 					<input
 						id={id}
@@ -61,6 +69,8 @@ export default class NumberInput extends Component {
 						onChange={e => { this.props.onChange(e); this.handleError(e)}}
 						onFocus={() => this.setState({focussed: true})}
 						onBlur={() => this.setState({ focussed: false })}
+						onKeyPress={this.preventMinus}
+						inputprops={{minlength:5, maxLength: 20}}
 					/>
 				</div>
 			</>
